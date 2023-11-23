d16f59bac5d870932180ffb15ab01ecc37502f32
gbri-dev <gabriel.camposdasilva@hotmail.com>
2023-11-23 17:04:40 -0300

    chore: watch less

--- gruntjs/Gruntfile.js
+++ gruntjs/Gruntfile.js
@@ -15,10 +15,17 @@ module.exports = function(grunt){
                     'dist/styles/main.min.css': 'src/stles/main.less'
                 }
             }   
+        },
+        watch: {
+            less: {
+                files: ['src/styles/**/**.less'],
+                tasks: ['less:development']
+            }
         }
     })
 
     grunt.loadNpmTasks('grunt-contrib-less')
-    grunt.registerTask('default', ['less:development'])
+    grunt.loadNpmTasks('grunt-contrib-watch')
+    grunt.registerTask('default', ['watch'])
     grunt.registerTask('build', ['less:production'])
 } 
